b08f50689014cd3750cd0ef559239f1c1bb22d41
waestalice <alice.barreto890@gmail.com>
2024-03-02 00:26:39 -0300

    Add moviment and animations

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+   public Animator animator; 	
+   public CharacterController characterController;
+   public float speed = 1f;
+   public float turnSpeed = 1f;
+   public float gravity = -9.8f;
+   public float jumpSpeed = 15f;
+
+   private float vSpeed = 0f;
+
+   public KeyCode jumpKeyCode = KeyCode.Space;
+
+   [Header("Run Setup")]
+   public KeyCode keyRun = KeyCode.LeftShift;
+   public float speedRun = 1.5f;
+
+   void Update()
+   {
+	   transform.Rotate(0, Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime, 0);
+
+	   var inputAxisVertical = Input.GetAxis("Vertical");
+	   var speedVector = transform.forward * inputAxisVertical * speed;
+
+	   if (characterController.isGrounded)
+	   {
+		   vSpeed = 0;
+		   if (Input.GetKeyDown(jumpKeyCode))
+		   {
+			   vSpeed = jumpSpeed;
+		   }
+	   }
+
+	   vSpeed -= gravity * Time.deltaTime;
+	   speedVector.y = vSpeed;
+
+
+	   var isWalking = inputAxisVertical != 0;
+	   if (isWalking)
+	   {
+		   if (Input.GetKey(keyRun))
+		   {
+			   speedVector *= speedRun;
+			   animator.speed = speedRun;
+		   }
+		   else
+		   {
+			   animator.speed = 1;
+		   }
+	   }
+
+	   characterController.Move(speedVector * Time.deltaTime);
+
+	   animator.SetBool("Run", inputAxisVertical != 0);
+   }
+}
