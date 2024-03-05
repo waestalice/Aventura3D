ad09ade25dd36e81ffdc4c07ceddfd2e0f214c60
waestalice <alice.barreto890@gmail.com>
2024-03-04 22:38:18 -0300

    Add switch keys to the guns

--- Assets/Scripts/Player/PlayerAbilityShoot.cs
+++ Assets/Scripts/Player/PlayerAbilityShoot.cs
@@ -5,10 +5,11 @@ using UnityEngine.InputSystem;
 
 public class PlayerAbilityShoot : PlayerAbilityBase
 {
-    public GunBase gunBase;
+    public List<GunBase> guns;
     public Transform gunPosition;
 
     private GunBase _currentGun;
+    private int _currentGunIndex = 0;
 
     protected override void Init()
     {
@@ -16,26 +17,60 @@ public class PlayerAbilityShoot : PlayerAbilityBase
 
         CreateGun();
 
+        // Registra os métodos para serem chamados quando a tecla correspondente for pressionada
         inputs.Gameplay.Shoot.performed += ctx => StartShoot();
         inputs.Gameplay.Shoot.canceled += ctx => CancelShoot();
     }
 
     private void CreateGun()
     {
-        _currentGun = Instantiate(gunBase, gunPosition);
+        // Instancia a primeira arma da lista ao iniciar
+        _currentGun = Instantiate(guns[_currentGunIndex], gunPosition);
 
         _currentGun.transform.localPosition = _currentGun.transform.localEulerAngles = Vector3.zero;
     }
 
+    private void Update()
+    {
+        // Verifica se a tecla 1 foi pressionada para trocar para a primeira arma
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SwitchGun(0);
+        }
+        // Verifica se a tecla 2 foi pressionada para trocar para a segunda arma
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            SwitchGun(1);
+        }
+    }
+
+    private void SwitchGun(int index)
+    {
+        if (index < 0 || index >= guns.Count || index == _currentGunIndex)
+        return;
+
+        // Desativa a arma atual
+        _currentGun.StopShoot();
+        Destroy(_currentGun.gameObject);
+
+        // Atualiza o índice da arma atual
+        _currentGunIndex = index;
+
+        // Instancia a nova arma
+        _currentGun = Instantiate(guns[_currentGunIndex], gunPosition);
+        _currentGun.transform.localPosition = _currentGun.transform.localEulerAngles = Vector3.zero;
+    }
+
+
     private void StartShoot()
     {
         _currentGun.StartShoot();
-        Debug.Log(" Start Shoot");
+        Debug.Log("Start Shoot");
     }
     
     private void CancelShoot()
     {
-        Debug.Log("Cancel Shoot");
         _currentGun.StopShoot();
+        Debug.Log("Cancel Shoot");
     }
 }
